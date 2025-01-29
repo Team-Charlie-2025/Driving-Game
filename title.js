6ccfd471586a09e5e33b309f5845d66b984674bf
Joe <jdemore4@kent.edu>
2025-01-29 18:05:07 -0500

    Title and Car
    not linked but title has buttons you can click, outputs to console
    car drives
    f to boost
    s to brake
    wasd
    adjustable turning speeds
    hold shift to turn faster
    
    done using if  keypresssed (keycodes) functions from p5js to just adjust rectangle stats
    feel free to overall later

---
+++ title.js
@@ -0,0 +1,116 @@
+let buttons = [];
+let bgImage;
+
+function preload() {
+    bgImage = loadImage("https://i.imgur.com/hnKdHwZ.jpeg");
+}
+
+function setup() {
+    createCanvas(1920, 1080);
+    textAlign(CENTER, CENTER);
+    textFont("Comic Sans MS");
+
+    // creates buttons from button class /defined
+    buttons.push(
+        new Button("Play Game", width / 2, height / 2 + 200, () => startGame())
+    );
+    buttons.push(
+        new Button("Garage", width / 2, height / 2 + 270, () => showGarage())
+    );
+    buttons.push(
+        new Button("Leaderboard", width / 2, height / 2 + 410, () =>
+            showLeaderboard()
+        )
+    );
+    buttons.push(
+        new Button("Settings", width / 2, height / 2 + 340, () => showSettings())
+    );
+    buttons.push(
+        new Button("Exit", width / 2, height / 2 + 480, () => exitGame())
+    );
+}
+
+function draw() {
+    if (bgImage) {
+        background(bgImage);
+    } else {
+        background(30, 30, 30);
+    }
+
+    // title
+    fill(255);
+    textSize(48);
+    textFont("Comic Sans MS");
+    text("Top D-Racer", width / 2, 150);
+
+    // how to interact with buttons
+    for (let button of buttons) {
+        button.display();
+    }
+}
+
+class Button {
+    constructor(label, x, y, action) {
+        this.label = label;
+        this.x = x;
+        this.y = y;
+        this.width = 200;
+        this.height = 50;
+        this.action = action;
+    }
+
+    display() {
+        if (this.isMouseOver()) {
+            fill(100, 200, 255);
+        } else {
+            fill(200);
+        }
+        rectMode(CENTER);
+        rect(this.x, this.y, this.width, this.height, 10);
+
+        fill(0);
+        textSize(24);
+        text(this.label, this.x, this.y);
+    }
+
+    isMouseOver() {
+        return (
+            mouseX > this.x - this.width / 2 &&
+            mouseX < this.x + this.width / 2 &&
+            mouseY > this.y - this.height / 2 &&
+            mouseY < this.y + this.height / 2
+        );
+    }
+
+    handleClick() {
+        if (this.isMouseOver()) {
+            this.action();
+        }
+    }
+}
+
+function mousePressed() {
+    for (let button of buttons) {
+        button.handleClick();
+    }
+}
+
+function startGame() {
+    console.log("Play Game clicked");
+}
+
+function showGarage() {
+    console.log("Garage clicked");
+}
+
+function showSettings() {
+    console.log("Settings clicked");
+}
+
+function showLeaderboard() {
+    console.log("Leaderboard clicked");
+}
+
+function exitGame() {
+    console.log("Exit clicked");
+}
