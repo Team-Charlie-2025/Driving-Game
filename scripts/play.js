0e15f071b1c69dc64cf3a636e942a7ca2ed0b2fd
mahashakshuki <144592137+mahashakshuki@users.noreply.github.com>
2025-02-03 17:54:15 -0500

    timer
    timer-test : need fixing

--- scripts/play.js
+++ scripts/play.js
@@ -1,7 +1,13 @@
 // game logic
-function drawPlay() {
+function drawPlay()
+ {
   background(230);
 
   car.update();
   car.display();
+  
+let sec = DateObj.getSeconds();
+// Printing second
+console.log(sec);
+
 }
