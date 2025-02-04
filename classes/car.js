55874fe534023787adb798ff23ff7bb58202ba09
mahashakshuki <144592137+mahashakshuki@users.noreply.github.com>
2025-02-03 20:04:39 -0500

    timer

--- classes/car.js
+++ classes/car.js
@@ -1,8 +1,22 @@
+<<<<<<< Updated upstream
 function setup(){
+=======
+function setup()
+ {
+  createCanvas(windowWidth, windowHeight);
+>>>>>>> Stashed changes
   car = new Car(width / 2, height / 2, 50, 30);
 }
 
+function displayTimer(timer) 
+{
+  fill(0);
+  textSize(32);
+  text('Boost Timer: ' + timer, 10, 30);
+}
+
 class Car {
+<<<<<<< Updated upstream
     constructor(x, y, w, h) {
       this.x = x;
       this.y = y;
@@ -33,6 +47,43 @@ class Car {
             this.maxSpeed
           );
         }
+=======
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h;
+    this.speed = 0;
+    this.angle = 0;
+    this.acceleration = 0.1;
+    this.maxSpeed = 8;
+    this.friction = 0.05;
+    this.reverseSpeed = -4;
+    this.defaultColor = color(100, 100, 255); // Normal color (Blue)
+    this.boostColor = color(255, 50, 50); // Boosting color (Red)
+    this.currentColor = this.defaultColor;
+    this.boostTimer=0;
+  }
+
+  update() {
+    if (keyIsDown(87)) {
+      if (keyIsDown(70)) {
+        this.speed = constrain(
+          this.speed + this.acceleration * 2,
+          this.reverseSpeed * 2,
+          this.maxSpeed * 2
+        );
+        this.currentColor = this.boostColor; // Change to boost color
+        this.boostTimer++;
+      } else {
+        this.speed = constrain(
+          this.speed + this.acceleration,
+          this.reverseSpeed,
+          this.maxSpeed
+        );
+        this.currentColor = this.defaultColor; // Reset color when not boosting
+        this.boostTimer = 0
+>>>>>>> Stashed changes
       }
       let turnSpeed = 0.05;
       if (keyIsDown(65)) {
@@ -61,6 +112,7 @@ class Car {
   } else if (this.y > height) {
       this.y = 0;
   }
+<<<<<<< Updated upstream
     }
   
     display() {
@@ -72,3 +124,16 @@ class Car {
       pop();
     }
   }
+=======
+}
+
+/*function draw() {
+  background(220);
+  car.update();
+  car.display();
+  displayTimer(car.boostTimer);
+}*/
+
+
+
+>>>>>>> Stashed changes
--- scripts/play.js
+++ scripts/play.js
@@ -1,13 +1,23 @@
 // game logic
+<<<<<<< Updated upstream
 function drawPlay()
  {
+=======
+function drawPlay() 
+
+{
+>>>>>>> Stashed changes
   background(230);
 
   car.update();
   car.display();
+<<<<<<< Updated upstream
   
 let sec = DateObj.getSeconds();
 // Printing second
 console.log(sec);
 
+=======
+  displayTimer(car.boostTimer);
+>>>>>>> Stashed changes
 }
