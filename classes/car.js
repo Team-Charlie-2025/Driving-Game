381a4ac68bf3790505d66fdc617199ea74078819
Joe <jdemore4@kent.edu>
2025-02-13 09:01:44 -0500

    Refactored
    Big changes:
    Refactored all code to be instanced based sketches using new p5 sketch
    Adjusted html accordingly
    Created a garage
    Added images
    Adjusted hierarchy for images
    (we have cars, engine, tires) folders for them
    Added persistent data using json and local storage
    Stats (semi randomly assigned for balance reasons) added to wheels, engines
    All stats are now loaded into the car constructor so they are applied to the vehicle
    including turning speed accel max speed health etc

--- classes/car.js
+++ classes/car.js
@@ -1,105 +1,78 @@
+// classes/car.js
 class Car {
-  constructor(x, y) {
+  constructor(p, x, y, stats) {
+    this.p = p;
     this.x = x;
     this.y = y;
-    //this.width = w;
-    //this.height = h;
-    this.xTile = y/gridSize ; // THIS FUNCTION WILL NEED CHANGED WHEN MAP EXPANDS
-    this.yTile = x/gridSize ; // Gives the approximate location on the 2d map array
     this.speed = 0;
     this.angle = 0;
-    this.acceleration = 0.1;
-    this.maxSpeed = 8;
+    // creates stats as const to prevent modifications
+    const SAVED_STATS = Object.freeze({ ...stats });
+    this.acceleration = SAVED_STATS.acceleration;
+    this.maxSpeed = SAVED_STATS.maxSpeed;
     this.friction = 0.05;
     this.reverseSpeed = -4;
-    this.currentImage = cars[1];
-    //this.defaultColor = color(100, 100, 255); // Normal color (Blue)
-   //this.boostColor = color(255, 50, 50); // Boosting color (Red)
-    //this.currentColor = this.defaultColor;
+    this.currentImage = (typeof cars !== 'undefined' && cars[0]) ? cars[0] : null;
+    this.width = 50;
+    this.height = 30;
+    this.healthBar = SAVED_STATS.health;
   }
 
   update() {
-    if (keyIsDown(87)) {
-      if (keyIsDown(70)) {
-        if(this.speed < 0) {
-            this.speed = .01;
+    let p = this.p;
+    if (p.keyIsDown(87)) {
+      if (p.keyIsDown(70)) {
+        if (this.speed < 0) {
+          this.speed = 0.01;
         }
-        this.speed = constrain(
+        this.speed = p.constrain(
           this.speed + this.acceleration * 2,
           this.reverseSpeed * 2,
           this.maxSpeed * 2
         );
-        //this.currentColor = this.boostColor; // Change to boost color
       } else {
-        this.speed = constrain(
+        this.speed = p.constrain(
           this.speed + this.acceleration,
           this.reverseSpeed,
           this.maxSpeed
         );
-        //this.currentColor = this.defaultColor; // Reset color when not boosting
       }
     }
-
-    if (keyIsDown(83)) {
-      this.speed = constrain(this.speed - this.acceleration, this.reverseSpeed, this.maxSpeed);
-      //this.currentColor = this.defaultColor; // Reset color if reversing
+    if (p.keyIsDown(83)) {
+      this.speed = p.constrain(this.speed - this.acceleration, this.reverseSpeed, this.maxSpeed);
     }
-
-    let turnSpeed = 0.05;      
-    if (keyIsDown(65)) {
-      if (keyIsDown(16)) this.angle -= turnSpeed * 2;
+    let turnSpeed = 0.05;
+    if (p.keyIsDown(65)) {
+      if (p.keyIsDown(16)) this.angle -= turnSpeed * 2;
       else this.angle -= turnSpeed;
     }
-    if (keyIsDown(68)) {
-      if (keyIsDown(16)) this.angle += turnSpeed * 2;
+    if (p.keyIsDown(68)) {
+      if (p.keyIsDown(16)) this.angle += turnSpeed * 2;
       else this.angle += turnSpeed;
     }
-
-    if (!keyIsDown(87) && !keyIsDown(83)) {
+    if (!p.keyIsDown(87) && !p.keyIsDown(83)) {
       this.speed *= 1 - this.friction;
       if (Math.abs(this.speed) < 0.01) this.speed = 0;
-      //this.currentColor = this.defaultColor; // Reset color if not moving
     }
-    
-    // Calculates what tile the car will be on 
-    this.xTile = Math.floor((this.x + this.speed * cos(this.angle)) / gridSize) ;
-    this.yTile = Math.ceil((this.y + this.speed * sin(this.angle)) / gridSize) ;
-
-    if(map[this.yTile][this.xTile] instanceof Building) {
-        //this.x -= (this.speed * cos(this.angle));
-        //this.y -= (this.speed * sin(this.angle));
-        this.healthBar -= 10;
-        console.log(this.healthBar);
-        this.speed = this.speed / -2 ;
-    }
-    else { 
-        this.x += this.speed * cos(this.angle);
-        this.y += this.speed * sin(this.angle);
-    }
-
-    if (this.x < 0) this.x = width;
-    else if (this.x > width) this.x = 0;
-
-    if (this.y < 0) this.y = height;
-    else if (this.y > height) this.y = 0;
+    this.x += this.speed * p.cos(this.angle);
+    this.y += this.speed * p.sin(this.angle);
+    if (this.x < 0) this.x = p.width;
+    else if (this.x > p.width) this.x = 0;
+    if (this.y < 0) this.y = p.height;
+    else if (this.y > p.height) this.y = 0;
   }
 
   display() {
-    push();
-    translate(this.x, this.y);
-    rotate(this.angle);
-
+    let p = this.p;
+    p.push();
+    p.translate(this.x, this.y);
+    p.rotate(this.angle);
     if (this.currentImage) {
-      // (-16, -32) rear wheel drive
-      // (-48, -32) front? wheel
-      drawingContext.drawImage(this.currentImage, -32, -64, 128, 128);
-      
+      p.image(this.currentImage, -32, -64, 128, 128);
     } else {
-      fill(0, 0, 0);
-      rect(0, 0, this.width, this.height); // Fallback in case image fails
+      p.fill(0, 0, 0);
+      p.rect(0, 0, this.width, this.height);
     }
-
-    pop();
+    p.pop();
   }
 }
-
