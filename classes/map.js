7e0da8ddcae2def7ca801fbbdc1b1340b2ed90da
zschupba <168577812+zschupba@users.noreply.github.com>
2025-02-17 21:00:10 -0500

    Update map.js

--- classes/map.js
+++ classes/map.js
@@ -33,6 +33,21 @@ class Road {
   }
 }
 
+class Grass {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.color = 'green';
+    this.width = gridSize;
+    this.height = gridSize;
+  }
+
+  draw(p) {
+    p.fill(this.color);
+    p.noStroke();
+    p.rect(this.x, this.y, this.width, this.height);
+  }
+}
 function generateRandomMap(p, rows, cols) {
   map = [];
   for (let y = 0; y < rows; y++) {
@@ -94,10 +109,10 @@ function generateDevMap(p, rows, cols) {
       map[y][x] = new Road(x * gridSize, y * gridSize);
     }
   }
-  drawRect(p, 20, 6, 8, 8);
-  drawRect(p, 36, 6, 8, 8);
-  drawRect(p, 20, 22, 8, 8);
-  drawRect(p, 36, 22, 8, 8);
+  drawRectBuilding(p, 20, 6, 8, 8);
+  drawRectBuilding(p, 36, 6, 8, 8);
+  drawRectBuilding(p, 20, 22, 8, 8);
+  drawRectBuilding(p, 36, 22, 8, 8);
 
   drawAngle45(p, 45, 13, 58, 0, 4);
   drawAngle45(p, 45, 25, 58, 8, 4);
@@ -105,9 +120,9 @@ function generateDevMap(p, rows, cols) {
   drawAngledBuilding(p, 19, 24, 0, 29, 4);
 }
 
-function drawRect(p, xStart, yStart, xLength, yLength) {
-  for (let y = yStart; y < yStart + yLength; y++) {
-    for (let x = xStart; x < xStart + xLength; x++) {
+function drawRectBuilding(p, xStart, yStart, xEnd, yEnd) {
+  for (let y = yStart; y < yEnd; y++) {
+    for (let x = xStart; x < xEnd; x++) {
       if (map[y] && map[y][x] !== undefined) {
         map[y][x] = new Building(x * gridSize, y * gridSize);
       }
@@ -162,6 +177,113 @@ function drawAngledBuilding(p, xStart, yStart, xEnd, yEnd, width) {
   }
 }
 
+//############################ Procedural Map ##############################
+// This is the beginning of generation, starts with blank map of grass, adds roads, then takes grass and fills it with buildings
+function generateGenMap(p,rows,cols){
+  for(let y=0; y<rows; y++){
+    map[y] = [];
+    for(let x=0; x<cols; x++){
+      map[y][x] = new Grass(x*gridSize, y*gridSize);
+    }
+  }
+  drawRectRoad(p,12,1,25,420);
+  drawAngledRoad(p,20,15,420,42,11);
+  drawBezierRoad(p,50,18,70,50,18,50,9);
+  drawAngledRoad(p,40,47,120,119,9);
+  drawRectRoad(p,119,22,130,252);
+  //drawBezierRoad(10,10,30,30,40,5,6);
+
+  //drawRectBuilding(27,25,47,39);
+  //drawRectBuilding(27,57,35,97);
+  //drawRowOfBuildings(1,1,12,101,8);
+  fillBigBuildings(p);
+  fillBuildingsDynamically(p);
+  drawBezierRoad(p,20,105,80,100,75,75,9)
+  
+  
+}
+
+function drawRectRoad(p,xStart,yStart,xEnd,yEnd){
+  for(let y=yStart; y<yEnd; y++){
+    for(let x=xStart; x<xEnd; x++){
+      if(map[y][x] !== undefined){
+        map[y][x] = new Road(x*gridSize, y*gridSize);
+        }
+      }
+  }
+}
+
+// This function only works if its not a 45 angle
+function drawAngledRoad(p,xStart, yStart, xEnd, yEnd, width) {
+
+  let dx = Math.abs(xEnd - xStart);
+  let dy = Math.abs(yEnd - yStart);
+  let stepX = xStart < xEnd ? 1 : -1;
+  let stepY = yStart < yEnd ? 1 : -1;
+
+  let err = dx - dy;
+  let x = xStart;
+  let y = yStart;
+
+  // Iterate using Bresenham-like logic to move along the slope
+  while (x !== xEnd || y !== yEnd) {
+    // Apply width perpendicular to the slope
+    for (let j = -Math.floor(width / 2); j <= Math.floor(width / 2); j++) {
+      let perpX = (dy > dx) ? j : 0; // Offset horizontally for steep slopes
+      let perpY = (dx > dy) ? j : 0; // Offset vertically for shallow slopes
+
+      let buildX = x + perpX;
+      let buildY = y + perpY;
+
+      if (map[buildY] && map[buildY][buildX] !== undefined) {
+        map[buildY][buildX] = new Road(buildX * gridSize, buildY * gridSize);
+      }
+    }
+
+    // Bresenham step to follow the line
+    let e2 = 2 * err;
+    if (e2 > -dy) { err -= dy; x += stepX; }
+    if (e2 < dx) { err += dx; y += stepY; }
+  }
+}
+
+//Need to start it inside the previous road
+function drawBezierRoad(p,x0, y0, x1, y1, x2, y2, width) {
+  let numSteps = 1000; // Increase number when turn has 
+  let prevX = x0, prevY = y0;
+
+  for (let i = 0; i <= numSteps; i++) {
+    let t = i / numSteps;
+    
+    // Quadratic Bézier formula
+    let x = (1 - t) * (1 - t) * x0 + 2 * (1 - t) * t * x1 + t * t * x2;
+    let y = (1 - t) * (1 - t) * y0 + 2 * (1 - t) * t * y1 + t * t * y2;
+
+    // Compute the tangent vector (direction of curve at this point)
+    let tangentX = x - prevX;
+    let tangentY = y - prevY;
+    let length = Math.sqrt((tangentX * tangentX) + (tangentY * tangentY));
+    
+    // Normalize tangent to get a perpendicular direction
+    let perpX = -tangentY / length;
+    let perpY = tangentX / length;
+
+    // Apply width consistently along the curve
+    for (let j = -Math.floor(width / 2); j <= Math.floor(width / 2); j++) {
+      let buildX = Math.round(x + j * perpX);
+      let buildY = Math.round(y + j * perpY);
+
+      if (map[buildY] && map[buildY][buildX] !== undefined) {
+        map[buildY][buildX] = new Road(buildX * gridSize, buildY * gridSize);
+      }
+    }
+
+    prevX = x;
+    prevY = y;
+  }
+}
+
+
 function drawMap(p) {
   p.background(255);
   for (let row of map) {
@@ -170,3 +292,4 @@ function drawMap(p) {
     }
   }
 }
+
