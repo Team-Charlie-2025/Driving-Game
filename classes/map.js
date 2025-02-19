fc80f81f56bf46645088f8bf110e380f1b9efbaa
zschupba <168577812+zschupba@users.noreply.github.com>
2025-02-19 18:56:42 -0500

    Update map.js
    Remove old map drawing functions and organized the functions

--- classes/map.js
+++ classes/map.js
@@ -48,134 +48,28 @@ class Grass {
     p.rect(this.x, this.y, this.width, this.height);
   }
 }
-function generateRandomMap(p, rows, cols) {
-  map = [];
-  for (let y = 0; y < rows; y++) {
-    map[y] = [];
-    for (let x = 0; x < cols; x++) {
-      if (p.random() > 0.1) {
-        map[y][x] = new Road(x * gridSize, y * gridSize);
-      } else {
-        map[y][x] = new Building(x * gridSize, y * gridSize);
-      }
-      if (y > (rows / 2) + 2 && y < (rows / 2) - 2) {
-        map[y][x] = new Building(x * gridSize, y * gridSize);
+
+
+function drawMap(p, car) {
+  p.background(255);
+  p.translate(p.width / 2 - car.x, p.height / 2 - car.y); // Center view on car
+
+  let colsVisible = Math.ceil(p.windowWidth / gridSize) + 2; // Gets canvas size 
+  let rowsVisible = Math.ceil(p.windowHeight / gridSize) + 2;
+
+  let startX = Math.floor(car.x / gridSize) - Math.floor(colsVisible / 2);
+  let startY = Math.floor(car.y / gridSize) - Math.floor(rowsVisible / 2);
+
+  for (let y = startY; y < startY + rowsVisible; y++) {
+    for (let x = startX; x < startX + colsVisible; x++) {
+      if (map[y] && map[y][x]) { // Check if tile exists
+        map[y][x].draw(p);
       }
     }
   }
 }
 
-function generateMap(p, rows, cols) {
-  map = [];
-  for (let y = 0; y < rows; y++) {
-    map[y] = [];
-    for (let x = 0; x < cols; x++) {
-      map[y][x] = new Building(x * gridSize, y * gridSize);
-    }
-  }
-  let roadWidth = 3;
-  // Horizontal roads
-  for (let y = roadWidth; y < rows; y += roadWidth * 2) {
-    for (let x = 0; x < cols; x++) {
-      for (let z = 0; z < roadWidth; z++) {
-        if (y + z == rows) break;
-        else map[y + z][x] = new Road(x * gridSize, (y + z) * gridSize);
-      }
-    }
-  }
-  // Vertical roads
-  for (let x = roadWidth; x < cols; x += roadWidth * 2) {
-    for (let y = 0; y < rows; y++) {
-      for (let z = 0; z < roadWidth; z++) {
-        map[y][x + z] = new Road((x + z) * gridSize, y * gridSize);
-      }
-    }
-  }
-  // Diagonal roads
-  for (let x = 0; x < cols; x++) {
-    for (let y = 0; y < rows; y++) {
-      if (x - y <= roadWidth && y - x <= roadWidth) {
-        map[y][x] = new Road(x * gridSize, y * gridSize);
-      }
-    }
-  }
-}
 
-function generateDevMap(p, rows, cols) {
-  map = [];
-  for (let y = 0; y < rows; y++) {
-    map[y] = [];
-    for (let x = 0; x < cols; x++) {
-      map[y][x] = new Road(x * gridSize, y * gridSize);
-    }
-  }
-  drawRectBuilding(p, 20, 6, 8, 8);
-  drawRectBuilding(p, 36, 6, 8, 8);
-  drawRectBuilding(p, 20, 22, 8, 8);
-  drawRectBuilding(p, 36, 22, 8, 8);
-
-  drawAngle45(p, 45, 13, 58, 0, 4);
-  drawAngle45(p, 45, 25, 58, 8, 4);
-  drawAngledBuilding(p, 19, 10, 0, 16, 4);
-  drawAngledBuilding(p, 19, 24, 0, 29, 4);
-}
-
-function drawRectBuilding(p, xStart, yStart, xEnd, yEnd) {
-  for (let y = yStart; y < yEnd; y++) {
-    for (let x = xStart; x < xEnd; x++) {
-      if (map[y] && map[y][x] !== undefined) {
-        map[y][x] = new Building(x * gridSize, y * gridSize);
-      }
-    }
-  }
-}
-
-function drawAngle45(p, xStart, yStart, xEnd, yEnd, width) {
-  let stepX = xStart < xEnd ? 1 : -1;
-  let stepY = yStart < yEnd ? 1 : -1;
-  let length = Math.abs(xEnd - xStart);
-  for (let i = 0; i <= length; i++) {
-    let baseX = xStart + i * stepX;
-    let baseY = yStart + i * stepY;
-    for (let j = -Math.floor(width / 2); j <= Math.floor(width / 2); j++) {
-      let buildX = baseX + j;
-      let buildY = baseY;
-      if (map[buildY] && map[buildY][buildX] !== undefined) {
-        map[buildY][buildX] = new Building(buildX * gridSize, buildY * gridSize);
-      }
-    }
-  }
-}
-
-function drawAngledBuilding(p, xStart, yStart, xEnd, yEnd, width) {
-  let dx = Math.abs(xEnd - xStart);
-  let dy = Math.abs(yEnd - yStart);
-  let stepX = xStart < xEnd ? 1 : -1;
-  let stepY = yStart < yEnd ? 1 : -1;
-  let err = dx - dy;
-  let x = xStart;
-  let y = yStart;
-  while (x !== xEnd || y !== yEnd) {
-    for (let j = -Math.floor(width / 2); j <= Math.floor(width / 2); j++) {
-      let perpX = (dy > dx) ? j : 0;
-      let perpY = (dx > dy) ? j : 0;
-      let buildX = x + perpX;
-      let buildY = y + perpY;
-      if (map[buildY] && map[buildY][buildX] !== undefined) {
-        map[buildY][buildX] = new Building(buildX * gridSize, buildY * gridSize);
-      }
-    }
-    let e2 = 2 * err;
-    if (e2 > -dy) {
-      err -= dy;
-      x += stepX;
-    }
-    if (e2 < dx) {
-      err += dx;
-      y += stepY;
-    }
-  }
-}
 
 //############################ Procedural Map ##############################
 // This is the beginning of generation, starts with blank map of grass, adds roads, then takes grass and fills it with buildings
@@ -191,13 +85,23 @@ function generateGenMap(p,rows,cols){
   drawBezierRoad(p,50,18,70,50,18,50,9);
   drawAngledRoad(p,40,47,120,119,9);
   drawRectRoad(p,119,22,130,252);
+  let blockSize = 50;
+  for(let i=1; i<5; ++i){
+    drawRectRoad(p,119+(i*blockSize),5,128+(i*blockSize),252);   // Vertical roads
+    drawRectRoad(p,119,22+(i*blockSize),380,31+(i*blockSize));    // Horizontal Roads
+  }
+
+  drawBezierRoad(p,15,160,150,225,15,260,9);
+  drawAngledRoad(p,61,245,75,400,9);
+  drawAngledRoad(p,86,218,119,226,8);
   //drawBezierRoad(10,10,30,30,40,5,6);
 
   //drawRectBuilding(27,25,47,39);
   //drawRectBuilding(27,57,35,97);
   //drawRowOfBuildings(1,1,12,101,8);
-  fillBigBuildings(p);
-  fillBuildingsDynamically(p);
+  fillBigBuildings(p,1,200,130,400);
+  fillShopsDynamically(p,1,1,100,180);
+  fillBuildingsDynamically(p,120,1,400,250);
   drawBezierRoad(p,20,105,80,100,75,75,9)
   
   
@@ -247,9 +151,9 @@ function drawAngledRoad(p,xStart, yStart, xEnd, yEnd, width) {
   }
 }
 
-//Need to start it inside the previous road
+// Draws a curved road curving towards x1,y1 starting from x0).
 function drawBezierRoad(p,x0, y0, x1, y1, x2, y2, width) {
-  let numSteps = 1000; // Increase number when turn has 
+  let numSteps = 1000; // Increase number when turn has random grass spots
   let prevX = x0, prevY = y0;
 
   for (let i = 0; i <= numSteps; i++) {
@@ -284,21 +188,62 @@ function drawBezierRoad(p,x0, y0, x1, y1, x2, y2, width) {
 }
 
 
-function drawMap(p, car) {
-  p.background(255);
-  p.translate(p.width / 2 - car.x, p.height / 2 - car.y); // Center view on car
 
-  let colsVisible = Math.ceil(p.windowWidth / gridSize) + 2; // Gets canvas size 
-  let rowsVisible = Math.ceil(p.windowHeight / gridSize) + 2;
+// Draws Buildings given the cordinates 
 
-  let startX = Math.floor(car.x / gridSize) - Math.floor(colsVisible / 2);
-  let startY = Math.floor(car.y / gridSize) - Math.floor(rowsVisible / 2);
-
-  for (let y = startY; y < startY + rowsVisible; y++) {
-    for (let x = startX; x < startX + colsVisible; x++) {
-      if (map[y] && map[y][x]) { // Check if tile exists
-        map[y][x].draw(p);
+function drawRectBuilding(p, xStart, yStart, xEnd, yEnd) {
+  for (let y = yStart; y < yEnd; y++) {
+    for (let x = xStart; x < xEnd; x++) {
+      if (map[y] && map[y][x] !== undefined) {
+        map[y][x] = new Building(x * gridSize, y * gridSize);
       }
+    }
+  }
+}
+
+function drawAngle45Building(p, xStart, yStart, xEnd, yEnd, width) {
+  let stepX = xStart < xEnd ? 1 : -1;
+  let stepY = yStart < yEnd ? 1 : -1;
+  let length = Math.abs(xEnd - xStart);
+  for (let i = 0; i <= length; i++) {
+    let baseX = xStart + i * stepX;
+    let baseY = yStart + i * stepY;
+    for (let j = -Math.floor(width / 2); j <= Math.floor(width / 2); j++) {
+      let buildX = baseX + j;
+      let buildY = baseY;
+      if (map[buildY] && map[buildY][buildX] !== undefined) {
+        map[buildY][buildX] = new Building(buildX * gridSize, buildY * gridSize);
+      }
+    }
+  }
+}
+
+function drawAngledBuilding(p, xStart, yStart, xEnd, yEnd, width) {
+  let dx = Math.abs(xEnd - xStart);
+  let dy = Math.abs(yEnd - yStart);
+  let stepX = xStart < xEnd ? 1 : -1;
+  let stepY = yStart < yEnd ? 1 : -1;
+  let err = dx - dy;
+  let x = xStart;
+  let y = yStart;
+  while (x !== xEnd || y !== yEnd) {
+    for (let j = -Math.floor(width / 2); j <= Math.floor(width / 2); j++) {
+      let perpX = (dy > dx) ? j : 0;
+      let perpY = (dx > dy) ? j : 0;
+      let buildX = x + perpX;
+      let buildY = y + perpY;
+      if (map[buildY] && map[buildY][buildX] !== undefined) {
+        map[buildY][buildX] = new Building(buildX * gridSize, buildY * gridSize);
+      }
+    }
+    let e2 = 2 * err;
+    if (e2 > -dy) {
+      err -= dy;
+      x += stepX;
+    }
+    if (e2 < dx) {
+      err += dx;
+      y += stepY;
     }
   }
 }
