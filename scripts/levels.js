78ec1c92e0ba59ead1ed587432b3a257069814b6
Anna Waltz <anna@waltzmail.net>
2025-03-02 20:46:30 -0500

    Comments and Background Concept
    addition of comments to code added and a concept art for the title screen

--- scripts/levels.js
+++ scripts/levels.js
@@ -2,19 +2,18 @@ function LevelsSketch(p) {
     let buttons = [];
 
     p.preload = function() {
-
         bgImage = p.loadImage("graphics/mainbg.png");
         imgTitle = p.loadImage("graphics/title.png");
-        if(!globalsLoaded) loadGlobals(p);
+        loadSound(p); // no music on page currently
     };
 
     p.setup = function () {
         p.createCanvas(p.windowWidth, p.windowHeight);
         p.textAlign(p.CENTER, p.CENTER);
-        createLevelButtons(); //level options
+        createLevelButtons(); //level option buttons
         ExitIcon = new Button("ExitIcon", p.width - p .width * 0.05, p.height - p.height * 0.95, function () { 
             switchSketch(Mode.TITLE);
-          });
+          }); // x button
     
         // stop loading
         window.LoadingScreen.hide();
@@ -27,7 +26,7 @@ function LevelsSketch(p) {
     };
 
     p.draw = function () {
-        p.background(225, 240, 255);
+        p.background(100, 193, 232); //change background in future?
         ExitIcon.display(p);
      
         for (let btn of buttons) {
@@ -37,23 +36,23 @@ function LevelsSketch(p) {
     p.mousePressed = function () {
         for (let btn of buttons) {
           if (btn.isMouseOver(p)) {
-            //bgMusic.stop();
             btn.callback();
             break;
           }
         }
         if (ExitIcon.isMouseOver(p)) {
-            //bgMusic.stop();
             ExitIcon.callback();
           }
       };
       p.keyPressed = function () {
         if (p.keyCode === p.ESCAPE) {
-          switchSketch(Mode.TITLE);
+            switchSketch(Mode.TITLE);
         }
       };
 
-    function createLevelButtons () {
+    function createLevelButtons () { 
+        //basic implementation, all go to same play mode
+        //modify for different levels/maps
         buttons.push(
             new Button("EXTRA HARD", p.width / 2, p.height - p.height * 0.80, function () {
               switchSketch(Mode.PLAY); //CHANGE TO LEVELS
