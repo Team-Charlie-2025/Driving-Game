ba14668d67c1f1906782ab4d54d85b80ebb956cc
Anna Waltz <anna@waltzmail.net>
2025-03-24 13:46:29 -0400

    Enemy damage error
    enemies taking damage against user car hits

--- classes/enemy.js
+++ classes/enemy.js
@@ -81,6 +81,10 @@ class Enemy extends Car {
   
     onCollisionEnter(other) {
       //super.onCollisionEnter(other);
+      if (other instanceof Car) { //upon ememy collision
+        let damage = 10;
+        this.healthBar = Math.max(0, this.healthBar - damage);
+      }
       if (other instanceof Car && Date.now() - this.lastAttack > this.attackCooldown) {
         this.lastAttack = Date.now();
   
